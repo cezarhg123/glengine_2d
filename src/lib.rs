88f2cfef7dadef98c3ad11f05030ffcb17d33e1f
Cezarhg2007 <cezarhg.89.popo9@gmail.com>
2023-01-08 00:08:06 +0000

    added clear function

--- src/lib.rs
+++ src/lib.rs
@@ -34,3 +34,10 @@ pub fn draw_element(count: u32) {
         gl::DrawElements(gl::TRIANGLES, count as i32, gl::UNSIGNED_INT, 0 as *const _);
     }
 }
+
+pub fn clear(r: f32, g: f32, b: f32) {
+    unsafe {
+        gl::ClearColor(r, g, b, 1.0);
+        gl::Clear(gl::COLOR_BUFFER_BIT);
+    }
+}
